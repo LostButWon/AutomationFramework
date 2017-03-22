c543237621d7eb4f0d7ca74c5521099eae87f329
anup <anup.popy@gmail.com>
2017-03-22 10:14:55 +0530

    commitandi

--- src/exp1/SampleCode.java
+++ src/exp1/SampleCode.java
@@ -14,12 +14,9 @@ public class SampleCode
 
 	public static void main(String[] args) {
 		
-
 System.out.println("NEW FILE");
-
 System.out.println("git hub code");
 System.out.println("this is the second statement");
-
 		
 	}
 	
